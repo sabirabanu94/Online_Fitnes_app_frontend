6ab1f3c5aed3e425ae0370aca7b689c17193ae03
sabirabanu94 <sabirabanu94@gmail.com>
2025-02-23 20:49:01 +0530

    fourth

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,15 +2,26 @@
 import React, { useContext, useState } from 'react';
 import { AuthContext } from '../store/AuthContext';
 
-const LoginPage = () => {
-    const { login } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        login(email, password);
-    };
+const Login = () => {
+  const [userData, setUserData] = useState({ username: '', password: '' });
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('https://online-fitnes-app-backend-2.onrender.com/api/login', {
+      method: 'POST',
+      body: JSON.stringify(userData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
     return (
         <form onSubmit={handleSubmit}>
